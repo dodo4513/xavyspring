65a6235fb13c1449c6bbcd6de94299abf140c1ff
aroma <dodo45133@gmail.com>
2018-02-04 14:50:52 +0900

    Initial commit

---
+++ src/main/java/study/controller/HelloController.java
@@ -0,0 +1,13 @@
+package study.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "hello, My name is xavy";
+    }
+}
