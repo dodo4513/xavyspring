67e028456653acea7f03b76a19d68024a8535e44
aroma <dodo45133@gmail.com>
2018-04-01 14:15:17 +0900

    1. java config로 변경. 2. H2COnfiguration 파일 이름 변경

---
+++ src/main/java/study/configuration/H2Database.java
@@ -0,0 +1,16 @@
+package study.configuration;
+
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class H2Database {
+    @Bean
+    public ServletRegistrationBean h2servletRegistration() {
+        ServletRegistrationBean registration = new ServletRegistrationBean(new WebServlet());
+        registration.addUrlMappings("/console/*");
+        return registration;
+    }
+}
